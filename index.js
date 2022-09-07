e214f26c95c4e9c9bfa1c97d7d67e965df8b24f0
Hoàng Hải Dương <hoanghaiduong1711@gmail.com>
2022-09-07 10:25:32 +0700

    ss

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import User_routes from "./routes/User_routes.js";
@@ -6,9 +8,32 @@ import Profile_routes from "./routes/Profile_routes.js";
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+dotenv.config();
 
 app.use("/api/user", User_routes);
 app.use("/api/profile", Profile_routes);
 
-export { app};
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    connect();
+    console.log(`server is running on port ${PORT}`);
+});
+const connect = async () => {
+    try {
+        await mongoose
+            .connect(process.env.MONGO_URI)
+            .then(() => {
+                console.log("Connected to MongoDB successfully");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("Disconnectd", () => {
+    console.log("Disconnected from MongoDB");
+});
+
--- server.js
+++ server.js
@@ -1,29 +1,21 @@
-import {app} from "./index.js";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+// import {app} from "./index.js";
 
-const connect = async () => {
-  try {
-    await mongoose
-      .connect(process.env.MONGO_URI)
-      .then(() => {
-        console.log("Connected to MongoDB successfully");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } catch (error) {
-    throw error;
-  }
-};
+// const connect = async () => {
+//   try {
+//     await mongoose
+//       .connect(process.env.MONGO_URI)
+//       .then(() => {
+//         console.log("Connected to MongoDB successfully");
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-mongoose.connection.on("Disconnectd", () => {
-  console.log("Disconnected from MongoDB");
-});
+// mongoose.connection.on("Disconnectd", () => {
+//   console.log("Disconnected from MongoDB");
+// });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  connect();
-  console.log(`server is running on port ${PORT}`);
-});
