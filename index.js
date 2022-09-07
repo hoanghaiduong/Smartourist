c6efdef1c5075eaad1216b534b0eba436ac88cdb
Hoàng Hải Dương <hoanghaiduong1711@gmail.com>
2022-09-07 11:09:21 +0700

    commitss

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import User_routes from "./routes/User_routes.js";
 import Profile_routes from "./routes/Profile_routes.js";
+import * as swagger from "swagger-ui-express";
+import swaggerDocument from "./swaggerDocument.json" assert { type: "json" };
+console.log(swaggerDocument);
 const app = express();
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());                                     
 app.use(cors());
+app.use('/api-docs/v2', swagger.serve);
+app.get('/api-docs/v2', swagger.setup(swaggerDocument));
 
 
 app.use("/api/user", User_routes);
