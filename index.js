91cc986db6e4b7bb6f6de3d89a390bc05f3d55a6
Hoàng Hải Dương <hoanghaiduong1711@gmail.com>
2022-09-07 10:49:12 +0700

    ss

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import User_routes from "./routes/User_routes.js";
@@ -8,32 +6,9 @@ import Profile_routes from "./routes/Profile_routes.js";
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-dotenv.config();
+
 
 app.use("/api/user", User_routes);
 app.use("/api/profile", Profile_routes);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    connect();
-    console.log(`server is running on port ${PORT}`);
-});
-const connect = async () => {
-    try {
-        await mongoose
-            .connect(process.env.MONGO_URI)
-            .then(() => {
-                console.log("Connected to MongoDB successfully");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    } catch (error) {
-        throw error;
-    }
-};
-
-mongoose.connection.on("Disconnectd", () => {
-    console.log("Disconnected from MongoDB");
-});
-
+export { app};
